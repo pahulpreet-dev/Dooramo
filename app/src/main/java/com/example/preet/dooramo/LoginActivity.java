fde7aac0deb06d0964895d969a1a104389a0473b
Pahulpreet Singh <pahul0957@gmail.com>
2018-11-23 01:12:17 -0500

    Fixed the LoginActivity code. If there is no user in the database, then app will open ManagementHome activity by default.

--- app/src/main/java/com/example/preet/dooramo/LoginActivity.java
+++ app/src/main/java/com/example/preet/dooramo/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.preet.dooramo;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,12 +18,33 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gin);
         initComponents();
+        if(checkUserExistence()) {
+
+        } else {
+            Intent intent = new Intent(LoginActivity.this, ManagementHome.class);
+            finish();
+            //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, ServicesActivity.class));
             }
         });
+    }
+
+    private boolean checkUserExistence() {
+        DBHelper dbHelper = new DBHelper(LoginActivity.this);
+        SQLiteDatabase dbcall = dbHelper.getReadableDatabase();
+
+        String query = "select * from userAccount";
+
+        Cursor next = dbcall.rawQuery(query, null);
+        if(next.moveToNext()) {
+            return true;
+        } else
+            return false;
     }
 
     private void initComponents() {
