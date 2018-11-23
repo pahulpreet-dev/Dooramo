5e498fbeca0f55f56cf47c9eacd93e4991bd6c20
Pahulpreet Singh <pahul0957@gmail.com>
2018-11-23 02:20:09 -0500

    Update LoginActivity.java

--- app/src/main/java/com/example/preet/dooramo/LoginActivity.java
+++ app/src/main/java/com/example/preet/dooramo/LoginActivity.java
@@ -8,12 +8,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button loginBtn;
-    EditText username, password;
     //LoginActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,41 +23,20 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             Intent intent = new Intent(LoginActivity.this, ManagementHome.class);
             finish();
-            //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         }
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //startActivity(new Intent(LoginActivity.this, ServicesActivity.class));
-                if(checkValidLogin()) {
-                    Intent intent = new Intent(LoginActivity.this, ServicesActivity.class);
-                    finish();
-                    //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(intent);
-                }
+                startActivity(new Intent(LoginActivity.this, ServicesActivity.class));
             }
         });
-    }
-    private boolean checkValidLogin() {
-        DBHelper dbHelper = new DBHelper(LoginActivity.this);
-        SQLiteDatabase dbcall = dbHelper.getReadableDatabase();
-
-        String query = "select * from userAccount where username = '" + username.getText().toString()
-                + "' AND password = '" + password.getText().toString() + "'";
-
-        Cursor next = dbcall.rawQuery(query, null);
-        if(next.moveToNext()) {
-            return true;
-        } else {
-            username.setError("Invalid Username/Password");
-            return false;
-        }
     }
 
     private boolean checkUserExistence() {
         DBHelper dbHelper = new DBHelper(LoginActivity.this);
         SQLiteDatabase dbcall = dbHelper.getReadableDatabase();
+
         String query = "select * from userAccount";
 
         Cursor next = dbcall.rawQuery(query, null);
@@ -71,7 +48,5 @@ public class LoginActivity extends AppCompatActivity {
 
     private void initComponents() {
         loginBtn = findViewById(R.id.lgnBtn);
-        username = findViewById(R.id.unameeditText);
-        password = findViewById(R.id.passwordeditText2);
     }
 }
